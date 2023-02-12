7d6892416e06b94cb8da138a5c4af0993c8f2db2
Chris <disenchanted@live.ca>
2023-02-11 22:42:33 -0800

    Vendors

--- app/src/main/java/com/advice/schedule/ui/information/vendors/VendorsFragment.kt
+++ app/src/main/java/com/advice/schedule/ui/information/vendors/VendorsFragment.kt
@@ -6,9 +6,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import com.advice.core.utils.Response
+import com.advice.schedule.dObj
 import com.advice.schedule.models.local.Vendor
+import com.advice.ui.VendorsScreenView
 import com.shortstack.hackertracker.R
 import com.shortstack.hackertracker.databinding.FragmentRecyclerviewBinding
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
@@ -17,91 +22,23 @@ class VendorsFragment : Fragment() {
 
     private val viewModel by sharedViewModel<VendorsViewModel>()
 
-    private var _binding: FragmentRecyclerviewBinding? = null
-    private val binding get() = _binding!!
-
-    private lateinit var adapter: VendorsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentRecyclerviewBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        adapter = VendorsAdapter {
-            val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse(it.link))
-            binding.root.context.startActivity(intent)
-        }
-
-        binding.list.adapter = adapter
-        binding.toolbar.title = getString(R.string.partners_vendors)
-        binding.toolbar.setNavigationOnClickListener {
-            requireActivity().onBackPressed()
-        }
-
-        viewModel.getVendors().observe(viewLifecycleOwner) {
-            onResource(it)
-        }
-    }
-
-    private fun onResource(resource: Response<List<Vendor>>) {
-        when (resource) {
-            is Response.Init -> {
-                setProgressIndicator(active = false)
-                showInitView()
-            }
-            is Response.Loading -> {
-                setProgressIndicator(active = true)
-                adapter.submitList(emptyList())
-                hideViews()
-            }
-            is Response.Success -> {
-                setProgressIndicator(active = false)
-
-                if (resource.data.isNotEmpty()) {
-                    adapter.submitList(resource.data)
-                    hideViews()
-                } else {
-                    adapter.submitList(emptyList())
-                    showEmptyView()
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                val state = viewModel.getVendors().observeAsState()
+                val vendors = state.value?.dObj as? List<Vendor> ?: emptyList()
+                VendorsScreenView(vendors = vendors) {
+                    requireActivity().onBackPressed()
                 }
             }
-            is Response.Error -> {
-                setProgressIndicator(active = false)
-                showErrorView(resource.exception.message)
-            }
         }
     }
-
-    private fun setProgressIndicator(active: Boolean) {
-        binding.loadingProgress.visibility = if (active) View.VISIBLE else View.GONE
-    }
-
-    private fun showInitView() {
-        binding.emptyView.visibility = View.VISIBLE
-        binding.emptyView.showDefault()
-    }
-
-    private fun showEmptyView() {
-        binding.emptyView.visibility = View.VISIBLE
-        binding.emptyView.showError("Vendors not found")
-    }
-
-    private fun showErrorView(message: String?) {
-        binding.emptyView.visibility = View.VISIBLE
-        binding.emptyView.showError(message)
-    }
-
-    private fun hideViews() {
-        binding.emptyView.visibility = View.GONE
-    }
-
     companion object {
         fun newInstance() = VendorsFragment()
     }
---
+++ ui/src/main/java/com/advice/ui/VendorsScreenView.kt
@@ -0,0 +1,96 @@
+package com.advice.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import com.advice.schedule.models.local.Vendor
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VendorsScreenView(vendors: List<Vendor>, onBackPressed: () -> Unit) {
+    Scaffold(topBar = {
+        TopAppBar(title = { Text("Partners & Vendors") }, navigationIcon = {
+            IconButton(onClick = { onBackPressed() }) {
+                Icon(Icons.Default.ArrowBack, null)
+            }
+        })
+    }) {
+        VendorsScreenContent(vendors, modifier = Modifier.padding(it))
+    }
+}
+
+@Composable
+fun VendorsScreenContent(vendors: List<Vendor>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier) {
+        items(vendors) {
+            VendorCard(it.name, it.summary, hasLink = it.link != null)
+        }
+    }
+}
+
+@Composable
+fun VendorCard(title: String, description: String, hasLink: Boolean) {
+    Card(modifier = Modifier.padding(16.dp)) {
+        Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.End) {
+            Text(title, fontWeight = FontWeight.Bold, modifier = Modifier.fillMaxWidth())
+            Spacer(Modifier.height(8.dp))
+            Text(description)
+            if (hasLink) {
+                OutlinedButton(onClick = { /*TODO*/ }) {
+                    Text("Website")
+                }
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun VendorsScreenViewPreview() {
+    MaterialTheme {
+        VendorsScreenView(
+            listOf(
+                Vendor(
+                    -1,
+                    "360 Unicorn Team",
+                    "360 Unicorn Team consists of a large team of self talk hackers that are really good at like, hacking and stuff.",
+                    "google.com",
+                    partner = false
+                ),
+                Vendor(
+                    -1,
+                    "360 Unicorn Team",
+                    "360 Unicorn Team consists of a large team of self talk hackers that are really good at like, hacking and stuff.",
+                    null,
+                    partner = true
+                )
+            )
+        ) {
+            
+        }
+    }
+}
