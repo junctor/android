2533abf3e28d271ab7ad4ee7586ea9aacb6f8ee3
Chris <disenchanted@live.ca>
2023-02-13 18:22:12 -0800

    Map in Compose

--- app/src/main/java/com/advice/schedule/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/advice/schedule/ui/maps/MapsFragment.kt
@@ -4,96 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import com.advice.core.firebase.FirebaseConferenceMap
 import com.advice.core.utils.Response
+import com.advice.schedule.dObj
 import com.advice.schedule.models.local.Location
 import com.advice.schedule.ui.HackerTrackerViewModel
 import com.advice.schedule.utilities.Analytics
+import com.advice.ui.screens.MapsScreenView
 import com.shortstack.hackertracker.databinding.FragmentMapsBinding
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class MapsFragment : Fragment() {
 
-    private var _binding: FragmentMapsBinding? = null
-    private val binding get() = _binding!!
-
     private val viewModel by sharedViewModel<HackerTrackerViewModel>()
-
-    private val analytics: Analytics by inject()
-    private var isFirstLoad: Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMapsBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.tabLayout.apply {
-            tabGravity = com.google.android.material.tabs.TabLayout.GRAVITY_FILL
-            setupWithViewPager(binding.pager)
-        }
-
-        binding.toolbar.setNavigationOnClickListener {
-            requireActivity().onBackPressed()
-        }
-
-        viewModel.maps.observe(viewLifecycleOwner) {
-            if (it is Response.Success) {
-                val maps = it.data
-                when (maps.size) {
-                    0 -> {
-                        binding.tabLayout.visibility = View.GONE
-                        binding.emptyView.visibility = View.VISIBLE
-                    }
-                    1 -> {
-                        binding.tabLayout.visibility = View.GONE
-                        binding.emptyView.visibility = View.GONE
-                    }
-                    else -> {
-                        binding.tabLayout.visibility = View.VISIBLE
-                        binding.emptyView.visibility = View.GONE
-                    }
-                }
-                val adapter = PagerAdapter(childFragmentManager, maps)
-                binding.pager.adapter = adapter
-
-                if (isFirstLoad) {
-                    isFirstLoad = false
-                    showSelectedMap(maps)
+        return ComposeView(requireContext()).apply {
+            isTransitionGroup = true
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                val maps = viewModel.maps.observeAsState().value?.dObj as? List<FirebaseConferenceMap> ?: emptyList()
+                MapsScreenView(maps) {
+                    requireActivity().onBackPressed()
                 }
             }
         }
-
-        analytics.setScreen(Analytics.SCREEN_MAPS)
-    }
-
-    private fun showSelectedMap(maps: List<FirebaseConferenceMap>) {
-        val location = arguments?.getParcelable<Location>(EXTRA_LOCATION)
-        if (location != null) {
-            val position = maps.indexOfFirst { it.title == location.hotel }
-            if (position != -1)
-                binding.pager.currentItem = position
-        }
-    }
-
-    class PagerAdapter(fm: FragmentManager, private val maps: List<FirebaseConferenceMap>) :
-        FragmentStatePagerAdapter(fm) {
-
-        override fun getItem(position: Int) = MapFragment.newInstance(maps[position].file)
-
-        override fun getPageTitle(position: Int) = maps[position].title
-
-        override fun getCount() = maps.size
     }
 
     companion object {
---
+++ ui/src/main/java/com/advice/ui/screens/MapsScreenView.kt
@@ -0,0 +1,82 @@
+package com.advice.ui.screens
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.advice.core.firebase.FirebaseConferenceMap
+import com.github.barteksc.pdfviewer.PDFView
+import com.github.barteksc.pdfviewer.util.FitPolicy
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import java.io.File
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MapsScreenView(maps: List<FirebaseConferenceMap>, onBackPressed: () -> Unit) {
+    Scaffold(topBar = {
+        TopAppBar(title = { Text("Maps") }, navigationIcon = {
+            IconButton(onClick = onBackPressed) {
+                Icon(Icons.Default.ArrowBack, null)
+            }
+        })
+    }) {
+        MapsScreenContent(maps.mapNotNull { it.file }, Modifier.padding(it))
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun MapsScreenContent(files: List<File>, modifier: Modifier = Modifier) {
+    HorizontalPager(
+        files.size,
+        modifier.fillMaxSize()
+    ) {
+        val file = files[it]
+        PDFView(file, Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+private fun PDFView(file: File, modifier: Modifier) {
+    AndroidView(factory = { context ->
+        PDFView(context, null)
+    },
+        modifier = modifier,
+        update = { view ->
+            println("loading: $file")
+            view.fromFile(file)
+                .pageFitPolicy(FitPolicy.HEIGHT)
+                .onLoad { pages ->
+
+                }
+                .load()
+        })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MapsScreenViewPreview() {
+    MaterialTheme {
+        MapsScreenView(listOf(FirebaseConferenceMap("Map", "", File("/")))) {
+
+        }
+    }
+}
