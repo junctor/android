53b5d5310b87187b96e599f7062f254af6e90700
Chris <disenchanted@live.ca>
2023-02-25 15:50:02 -0800

    Update DaySelectorView.kt

--- ui/src/main/java/com/advice/ui/views/DaySelectorView.kt
+++ ui/src/main/java/com/advice/ui/views/DaySelectorView.kt
@@ -1,40 +1,31 @@
 package com.advice.ui.views
 
-import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.AnimationVector1D
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInParent
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import com.advice.ui.Colors
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -54,21 +45,22 @@ fun DaySelectorView(days: List<String>, start: Int, end: Int, onDaySelected: (St
         Modifier
             .height(IntrinsicSize.Min)
     ) {
-        Box(
-            modifier = Modifier
-                .padding(horizontal = 8.dp, vertical = 16.dp)
-                .width(with(LocalDensity.current) { (endPosition.value - startPosition.value).toDp() })
-                .fillMaxHeight()
-                .offset(with(LocalDensity.current) { startPosition.value.toDp() }, 0.dp)
-//                .clip(CircleShape)
-                //.background(Color.Blue)
-                .border(3.dp, Color.Black)
-                .animateContentSize()
-        )
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .horizontalScroll(rememberScrollState())
+                .drawBehind {
+                    val verticalPadding = size.height * 0.25f
+                    translate(left = startPosition.value, top = verticalPadding) {
+                        drawRoundRect(
+                            Colors.controlActive, size = Size(
+                                width = endPosition.value - startPosition.value,
+                                height = size.height - (verticalPadding * 2)
+                            ),
+                            cornerRadius = CornerRadius(60f)
+                        )
+                    }
+                }
                 .padding(horizontal = 8.dp, vertical = 16.dp)
         ) {
             days.forEachIndexed { index, it ->
@@ -92,7 +84,6 @@ fun DaySelectorView(days: List<String>, start: Int, end: Int, onDaySelected: (St
                 }
             }
         }
-
     }
 }
 
