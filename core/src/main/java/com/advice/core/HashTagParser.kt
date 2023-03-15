dd0b29778aecb6726c0dfa12fc5943015e0ba8a7
Chris <disenchanted@live.ca>
2023-03-14 19:40:07 -0700

    Update HashTagParser.kt

--- core/src/main/java/com/advice/core/HashTagParser.kt
+++ core/src/main/java/com/advice/core/HashTagParser.kt
@@ -5,7 +5,7 @@ import com.advice.core.utils.Url
 object HashTagParser {
 
     fun findAllHashTags(text: String): List<Url> {
-        val hashTags = Regex("(#.*?) ").findAll(text)
+        val hashTags = Regex("(#[^.]+?)[ .]").findAll(text)
         val urls = mutableListOf<Url>()
         for (hashTag in hashTags) {
             urls.add(Url(hashTag.groupValues[1], hashTag.range.first, hashTag.range.last))
