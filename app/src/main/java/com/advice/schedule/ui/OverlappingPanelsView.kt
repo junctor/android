d957d0a23d267b2fb9606b5e9e409f357cd15e10
advice <chrisporter0111@gmail.com>
2023-05-31 22:29:15 -0700

    Using state for OverlappingPanels

--- app/src/main/java/com/advice/schedule/ui/OverlappingPanelsView.kt
+++ app/src/main/java/com/advice/schedule/ui/OverlappingPanelsView.kt
@@ -1,5 +1,6 @@
 package com.advice.schedule.ui
 
+import android.os.Parcelable
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
@@ -11,6 +12,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -27,28 +30,43 @@ import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.core.math.MathUtils.clamp
 import com.advice.ui.theme.ScheduleTheme
+import kotlinx.parcelize.Parcelize
 import kotlin.math.roundToInt
 
 private const val GUTTER_SIZE = 56
 private const val GUTTER_PADDING = 8
 
-
-sealed class Panel {
+@Parcelize
+sealed class Panel : Parcelable {
     object Left : Panel()
     object Right : Panel()
     object Main : Panel()
 }
 
-sealed class LockState {
+@Parcelize
+sealed class LockState : Parcelable {
     object Open : LockState()
     object Close : LockState()
     object Unlocked : LockState()
 }
 
-sealed class SwipeDirection {
+@Parcelize
+sealed class SwipeDirection : Parcelable {
     object Left : SwipeDirection()
     object Right : SwipeDirection()
+    object None : SwipeDirection()
 }
+
+@Parcelize
+data class PanelState(
+    val panel: Panel = Panel.Main,
+    val lockState: LockState = LockState.Unlocked,
+    val swipeDirection: SwipeDirection = SwipeDirection.None,
+
+    val isDragging: Boolean = false,
+    val offsetX: Float = 0f,
+    val alpha: Float = 1f,
+) : Parcelable
 
 @Composable
 fun OverlappingPanelsView(
@@ -58,22 +76,17 @@ fun OverlappingPanelsView(
     modifier: Modifier = Modifier,
     onPanelChangedListener: ((Panel) -> Unit)? = null,
 ) {
+    var state by rememberSaveable { mutableStateOf(PanelState()) }
+
     var size by remember { mutableStateOf(IntSize.Zero) }
-
-    var selectedPanel by remember { mutableStateOf<Panel>(Panel.Main) }
-
-    var isDragging by remember { mutableStateOf(false) }
-    var offsetX by rememberSaveable { mutableStateOf(0f) }
-    val animatedOffset by animateFloatAsState(offsetX)
-
-    var alpha by rememberSaveable { mutableStateOf(1f) }
-    val animatedAlpha by animateFloatAsState(alpha)
-
     val gutterSize = with(LocalDensity.current) { GUTTER_SIZE.dp.toPx() }
 
-    var dragStartX by remember { mutableStateOf(0f) }
-    var dragStartTime by remember { mutableStateOf(0L) }
-    var dragEndX by remember { mutableStateOf(0f) }
+    val animatedOffset by animateFloatAsState(state.offsetX)
+    val animatedAlpha by animateFloatAsState(state.alpha)
+
+    var dragStartX by remember { mutableFloatStateOf(0f) }
+    var dragStartTime by remember { mutableLongStateOf(0L) }
+    var dragEndX by remember { mutableFloatStateOf(0f) }
 
 
     // A container that has 3 children, the first is the left panel, the second is the main content, and the the third is the right panel. The middle panel can slide to the left or right to reveal the left or right panel.
@@ -86,67 +99,78 @@ fun OverlappingPanelsView(
             .pointerInput(Unit) {
                 detectHorizontalDragGestures(
                     onDragStart = { start ->
-                        isDragging = true
+                        state = state.copy(isDragging = true)
+
                         dragStartX = start.x
                         dragEndX = start.x
                         dragStartTime = System.currentTimeMillis()
                     },
                     onHorizontalDrag = { change, dragAmount ->
-                        alpha = setAlphaState(isDragging, selectedPanel)
                         change.consume()
-                        offsetX += dragAmount
+
                         dragEndX += dragAmount
-                        offsetX = clamp(
-                            offsetX,
-                            -size.width + gutterSize,
-                            size.width - gutterSize
+
+                        state = state.copy(
+                            offsetX = clamp(
+                                state.offsetX + dragAmount,
+                                -size.width + gutterSize,
+                                size.width - gutterSize,
+                            ),
+                            alpha = 1f,
                         )
                     },
                     onDragEnd = {
-                        isDragging = false
+                        state = state.copy(isDragging = false)
 
                         val dragEndTime = System.currentTimeMillis()
                         val dragDuration = dragEndTime - dragStartTime
                         val dragDistance = dragEndX - dragStartX
                         val velocity = dragDistance / dragDuration * 1000
 
-                        when {
-                            offsetX > size.width / 2 -> {
-                                selectedPanel = Panel.Left
-                                offsetX = size.width - gutterSize
-                                onPanelChangedListener?.invoke(selectedPanel)
-                            }
-
-                            offsetX < -size.width / 2 -> {
-                                selectedPanel = Panel.Right
-                                offsetX = -size.width + gutterSize
-                                onPanelChangedListener?.invoke(selectedPanel)
-                            }
-
-                            selectedPanel != Panel.Main -> {
-                                selectedPanel = Panel.Main
-                                offsetX = 0f
-                                onPanelChangedListener?.invoke(selectedPanel)
-                                return@detectHorizontalDragGestures
-                            }
-                        }
-
-
                         val swipeDirection = when {
                             velocity > 1000f -> SwipeDirection.Right
                             velocity < -1000f -> SwipeDirection.Left
-                            else -> return@detectHorizontalDragGestures
+                            else -> SwipeDirection.None
                         }
 
-                        when (selectedPanel) {
+                        if (swipeDirection == SwipeDirection.None) {
+                            when {
+                                state.offsetX > size.width / 2 -> {
+                                    state = state.copy(
+                                        panel = Panel.Left,
+                                        offsetX = size.width - gutterSize
+                                    )
+                                    onPanelChangedListener?.invoke(state.panel)
+                                }
+
+                                state.offsetX < -size.width / 2 -> {
+                                    state = state.copy(
+                                        panel = Panel.Right,
+                                        offsetX = -size.width + gutterSize
+                                    )
+                                    onPanelChangedListener?.invoke(state.panel)
+                                }
+
+                                else -> {
+                                    state = state.copy(panel = Panel.Main, offsetX = 0f)
+                                    onPanelChangedListener?.invoke(state.panel)
+                                }
+                            }
+                            return@detectHorizontalDragGestures
+                        }
+
+                        when (state.panel) {
                             Panel.Left -> when (swipeDirection) {
                                 SwipeDirection.Left -> {
-                                    selectedPanel = Panel.Main
-                                    offsetX = 0f
-                                    onPanelChangedListener?.invoke(selectedPanel)
+                                    state = state.copy(panel = Panel.Main, offsetX = 0f)
+                                    onPanelChangedListener?.invoke(state.panel)
                                 }
 
                                 SwipeDirection.Right -> {
+                                    // no-op
+                                }
+
+                                SwipeDirection.None -> {
                                     // no-op
                                 }
                             }
@@ -154,45 +178,58 @@ fun OverlappingPanelsView(
                             Panel.Main -> {
                                 when (swipeDirection) {
                                     SwipeDirection.Left -> {
-                                        selectedPanel = Panel.Right
-                                        offsetX = -size.width + gutterSize
-                                        onPanelChangedListener?.invoke(selectedPanel)
+                                        state = state.copy(
+                                            panel = Panel.Right,
+                                            offsetX = -size.width + gutterSize
+                                        )
+
+                                        onPanelChangedListener?.invoke(state.panel)
                                     }
 
                                     SwipeDirection.Right -> {
-                                        selectedPanel = Panel.Left
-                                        offsetX = size.width - gutterSize
-                                        onPanelChangedListener?.invoke(selectedPanel)
+                                        state = state.copy(
+                                            panel = Panel.Left,
+                                            offsetX = size.width - gutterSize
+                                        )
+                                        onPanelChangedListener?.invoke(state.panel)
+                                    }
+
+                                    SwipeDirection.None -> {
+                                        // no-op
                                     }
                                 }
                             }
 
                             Panel.Right -> when (swipeDirection) {
                                 SwipeDirection.Right -> {
-                                    selectedPanel = Panel.Main
-                                    offsetX = 0f
-                                    onPanelChangedListener?.invoke(selectedPanel)
+                                    state = state.copy(panel = Panel.Main, offsetX = 0f)
+                                    onPanelChangedListener?.invoke(state.panel)
                                 }
 
                                 SwipeDirection.Left -> {
                                     // no-op
                                 }
+
+                                SwipeDirection.None -> {
+                                    // no-op
+                                }
                             }
                         }
-                        alpha = setAlphaState(isDragging, selectedPanel)
+                        state =
+                            state.copy(alpha = if (state.isDragging || state.panel == Panel.Main) 1.0f else 0.65f)
                     },
                 )
             }
             .pointerInput(Unit) {
                 detectTapGestures(onTap = { offset ->
                     //tryAwaitRelease()
-                    when (selectedPanel) {
+                    when (state.panel) {
                         Panel.Left -> {
                             if (offset.x > size.width - gutterSize) {
-                                selectedPanel = Panel.Main
-                                offsetX = 0f
-                                alpha = setAlphaState(isDragging, selectedPanel)
-                                onPanelChangedListener?.invoke(selectedPanel)
+                                state = state.copy(panel = Panel.Main, offsetX = 0f)
+                                state =
+                                    state.copy(alpha = if (state.isDragging || state.panel == Panel.Main) 1.0f else 0.65f)
+                                onPanelChangedListener?.invoke(state.panel)
                             }
                         }
 
@@ -202,10 +239,10 @@ fun OverlappingPanelsView(
 
                         Panel.Right -> {
                             if (offset.x < gutterSize) {
-                                selectedPanel = Panel.Main
-                                offsetX = 0f
-                                alpha = setAlphaState(isDragging, selectedPanel)
-                                onPanelChangedListener?.invoke(selectedPanel)
+                                state = state.copy(panel = Panel.Main, offsetX = 0f)
+                                state =
+                                    state.copy(alpha = if (state.isDragging || state.panel == Panel.Main) 1.0f else 0.65f)
+                                onPanelChangedListener?.invoke(state.panel)
                             }
                         }
                     }
@@ -247,7 +284,7 @@ fun OverlappingPanelsView(
 
 private fun setAlphaState(
     isDragging: Boolean,
-    selectedPanel: Panel
+    selectedPanel: Panel,
 ): Float {
     return if (isDragging || selectedPanel == Panel.Main) 1.0f else 0.65f
 }
