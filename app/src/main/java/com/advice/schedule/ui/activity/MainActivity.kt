d1727efdd3dd252b15802ab6bc1fa52059c6b1eb
advice <chrisporter0111@gmail.com>
2023-06-11 22:31:45 -0700

    Update MainActivity.kt

--- app/src/main/java/com/advice/schedule/ui/activity/MainActivity.kt
+++ app/src/main/java/com/advice/schedule/ui/activity/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
