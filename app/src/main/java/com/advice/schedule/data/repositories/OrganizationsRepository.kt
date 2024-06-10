9a8e5a25a776c96148007e02677a3d920479d0ff
advice <chrisporter0111@gmail.com>
2024-06-09 20:58:20 -0700

    Update OrganizationsRepository.kt

--- app/src/main/java/com/advice/schedule/data/repositories/OrganizationsRepository.kt
+++ app/src/main/java/com/advice/schedule/data/repositories/OrganizationsRepository.kt
@@ -4,15 +4,25 @@ import com.advice.core.local.Organization
 import com.advice.data.sources.OrganizationsDataSource
 import com.advice.data.sources.VendorsDataSource
 import com.advice.data.sources.VillagesDataSource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.shareIn
+import timber.log.Timber
 
 class OrganizationsRepository(
     private val organizationsDataSource: OrganizationsDataSource,
 ) {
+
     suspend fun getOrganization(id: Long): Organization? {
         return organizations.first().find { it.id == id }
     }
 
-    val organizations = organizationsDataSource.get()
+    val organizations = organizationsDataSource.get().shareIn(
+        scope = CoroutineScope(Dispatchers.Default),
+        started = SharingStarted.Lazily,
+        replay = 1
+    )
 }
