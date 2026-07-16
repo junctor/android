126e5b00776048e32f9b990bb7007baf710df225
ace42588 <2062126+ace42588@users.noreply.github.com>
2026-07-16 15:51:06 -0400

    fix: replaced TODO()s

--- data/src/main/java/com/advice/data/InMemoryBookmarkedDataSourceImpl.kt
+++ data/src/main/java/com/advice/data/InMemoryBookmarkedDataSourceImpl.kt
@@ -11,37 +11,49 @@ class InMemoryBookmarkedDataSourceImpl : BookmarkedElementDataSource {
 
     private val flow = MutableStateFlow<List<Bookmark>>(emptyList())
 
-    private val map = mutableMapOf<Long, Bookmark>()
+    private val map = mutableMapOf<String, Bookmark>()
 
     override fun get() = flow
 
     override suspend fun bookmark(tag: Tag, isBookmarked: Boolean) {
+        bookmark(key = "tag:${tag.id}", Bookmark.TagBookmark(tag.id.toString(), value = true), isBookmarked)
+    }
+
+    override suspend fun bookmark(content: Content, isBookmarked: Boolean) {
+        bookmark(
+            key = "content:${content.id}",
+            Bookmark.ContentBookmark(content.id.toString(), value = true),
+            isBookmarked,
+        )
+    }
+
+    override suspend fun bookmark(session: Session, isBookmarked: Boolean) {
+        bookmark(
+            key = "session:${session.id}",
+            Bookmark.SessionBookmark(session.id.toString(), value = true),
+            isBookmarked,
+        )
+    }
+
+    private suspend fun bookmark(key: String, bookmark: Bookmark, isBookmarked: Boolean) {
         if (isBookmarked) {
-            map[tag.id] = Bookmark.TagBookmark(tag.id.toString(), value = true)
+            map[key] = bookmark
         } else {
-            map.remove(tag.id)
+            map.remove(key)
         }
         flow.emit(map.values.toList())
     }
 
-    override suspend fun bookmark(content: Content, isBookmarked: Boolean) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun bookmark(session: Session, isBookmarked: Boolean) {
-        TODO("Not yet implemented")
-    }
-
     override suspend fun isBookmarked(content: Content): Boolean {
-        TODO("Not yet implemented")
+        return map.containsKey("content:${content.id}")
     }
 
     override suspend fun isBookmarked(session: Session): Boolean {
-        TODO("Not yet implemented")
+        return map.containsKey("session:${session.id}")
     }
 
     override suspend fun isBookmarked(tag: Tag): Boolean {
-        TODO("Not yet implemented")
+        return map.containsKey("tag:${tag.id}")
     }
 
     override suspend fun clear() {
